8034275fad55398445ff49ed526918ee3083ca3e
Jessica Andersen <jessmand@mit.edu>
2014-04-10 18:57:14 -0400

    before the color revision...

--- frame.js
+++ frame.js
@@ -1,12 +1,14 @@
 $(function() {
     
     var sidebarHeight = function() {
-        var height = $(window).height()-$('.row-top').height() - 10;
+        var height = $(window).height()-$('.top-banner').height() - 10;
         console.log(height);
         $('.sidebar').height(height);
     }
-    
+    $('.row-top').height($('.top-banner').height());
+    //$('.sidebar').css("top", $('.top-banner').height());
     sidebarHeight();
+    
     
     $(window).resize(sidebarHeight);
     
