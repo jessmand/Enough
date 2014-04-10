08b999dc1833931db971adadd5c79bab9a89cb71
Jessica Andersen <jessmand@mit.edu>
2014-04-10 19:56:01 -0400

    new colors! new positioning!

--- frame.js
+++ frame.js
@@ -4,6 +4,10 @@ $(function() {
         var height = $(window).height()-$('.top-banner').height() - 10;
         console.log(height);
         $('.sidebar').height(height);
+        var contentLeft = $('.sidebar').width()+20;
+        var contentWidth = $(window).width()-contentLeft;
+        $('.content').css('margin-left', contentLeft);
+        $('.content').width(contentWidth);
     }
     $('.row-top').height($('.top-banner').height());
     //$('.sidebar').css("top", $('.top-banner').height());
