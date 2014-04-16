4a885186b8d471706ba28d0178ce01c773e41660
afreel <austin.freel@gmail.com>
2014-04-15 23:58:19 -0400

    Fixed alignment, added borders

---
+++ Steps.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$("#button1to2").on("click", function() {
+		location.href="./StepTwo.html";
+	});
+
+	$("#button2to3").on("click", function() {
+		location.href="./StepThree.html";
+	});
+
+	$("#buttonFinish").on("click", function() {
+		location.href="./myprogress.html";
+	});
+
+	$("#button2to1").on("click", function() {
+		location.href="./StepOne.html";
+	});
+
+	$("#button3to2").on("click", function() {
+		console.log("HI");
+		location.href="./StepTwo.html";
+	});
+});
