7920e1fa098381a4d922d7503d6eb76b2c9b1538
David Sessoms <dsessoms@Davids-MacBook-Pro-3.local>
2014-05-01 18:29:31 -0400

    added routing and updated frame to render as layout

---
+++ meteor/client/routing.js
@@ -0,0 +1,15 @@
+Router.map(function() {
+	this.route('myprogress', {path: "/", 
+		layoutTemplate: 'frame'
+	});
+
+	this.route('people', {
+		layoutTemplate: 'frame'
+	});
+
+	this.route('forums', {
+		layoutTemplate: 'frame'
+	})
+
+})
+
